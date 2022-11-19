d81d04f971b75ad671466fe98c56eb5c5041dfe7
Emanuele Ianni <emanuele.ianni@maersk.com>
2022-11-19 21:23:10 +0100

    initial stuff

---
+++ App.test.tsx
@@ -0,0 +1,18 @@
+import { expect, test} from '@jest/globals';
+import App from './App';
+import { render, fireEvent } from '@testing-library/react-native';
+
+test('given empty GroceryShoppingList, user can add an item to it', () => {
+  const { getByPlaceholderText, getByText, getAllByText } = render(
+    <App />
+  );
+
+  fireEvent.changeText(
+    getByPlaceholderText('Enter grocery item'),
+    'banana'
+  );
+  fireEvent.press(getByText('Add the item to list'));
+
+  const bananaElements = getAllByText('banana');
+  expect(bananaElements).toHaveLength(1); // expect 'banana' to be on the list
+});
---
+++ App.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  const [groceryItem, setGroceryItem] = useState('');
+  const [items, setItems] = useState([]);
+
+  const addNewItemToShoppingList = useCallback(() => {
+    setItems([groceryItem, ...items]);
+    setGroceryItem('');
+  }, [groceryItem, items]);
+  
+  return (
+    <>
+                <View style={styles.container}>
+      <TextInput
+        value={groceryItem}
+        placeholder="Enter grocery item"
+        onChangeText={(text) => setGroceryItem(text)}
+      />
+      <Button
+        title="Add the item to list"
+        onPress={addNewItemToShoppingList}
+      />
+      {items.map((item) => (
+        <Text key={item}>{item}</Text>
+      ))}
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
